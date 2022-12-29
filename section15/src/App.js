a43a3f1eb0c622386f01a62c4159231e059dd89c
2001Kaye <jhan117@naver.com>
2022-12-29 13:50:32 +0900

    Fix: 본인 Firebase API 링크로 수정

--- section15/src/App.js
+++ section15/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        "https://react-http-6b4a6.firebaseio.com/tasks.json"
+        "https://react-getting-started-d2fc4-default-rtdb.firebaseio.com/tasks.json"
       );
 
       if (!response.ok) {
--- section15/src/components/NewTask/NewTask.js
+++ section15/src/components/NewTask/NewTask.js
@@ -12,7 +12,7 @@ const NewTask = (props) => {
     setError(null);
     try {
       const response = await fetch(
-        "https://react-http-6b4a6.firebaseio.com/tasks.json",
+        "https://react-getting-started-d2fc4-default-rtdb.firebaseio.com/tasks.json",
         {
           method: "POST",
           body: JSON.stringify({ text: taskText }),
