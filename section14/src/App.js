f63491e917f869374c4d057ae30115336529e1d6
2001Kaye <jhan117@naver.com>
2022-12-29 10:11:00 +0900

    Feat: 사용자에게 데이터 상태 알려주기

--- section14/src/App.js
+++ section14/src/App.js
@@ -5,9 +5,11 @@ import "./App.css";
 import MoviesList from "./components/MoviesList";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [movies, setMovies] = useState([]);
 
   const fetchMoviesHandler = async () => {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -19,6 +21,7 @@ function App() {
     }));
 
     setMovies(transformedMovies);
+    setIsLoading(false);
   };
 
   return (
@@ -27,7 +30,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
