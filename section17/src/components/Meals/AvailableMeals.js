24bd05bc7388c989384e424f0273e6e87fcecac1
2001Kaye <jhan117@naver.com>
2022-12-30 15:32:12 +0900

    Feat: 오류 상태 추가

--- section17/src/components/Meals/AvailableMeals.js
+++ section17/src/components/Meals/AvailableMeals.js
@@ -8,12 +8,14 @@ import MealItem from "./MealItem/MealItem";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-getting-started-d2fc4-default-rtdb.firebaseio.com/meals.json"
       );
+      if (!response.ok) throw new Error("Something went wrong!");
       const data = await response.json();
 
       const loadedMeals = [];
@@ -30,13 +32,24 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.mealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
