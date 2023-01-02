79f7e2e040898bb0241f36283b6c65e7b3531642
2001Kaye <jhan117@naver.com>
2023-01-02 11:45:35 +0900

    Refactor: slice action 연결

--- section18/in-react/src/components/Counter.js
+++ section18/in-react/src/components/Counter.js
@@ -3,23 +3,25 @@ import { useSelector, useDispatch, connect } from "react-redux";
 
 import classes from "./Counter.module.css";
 
+import { counterActions } from "../store";
+
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
   const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 10 });
+    dispatch(counterActions.increase(10));
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
