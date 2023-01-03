29f61b0d991207204f5eadf2198168eb3e1e8448
2001Kaye <jhan117@naver.com>
2023-01-03 09:40:52 +0900

    Feat: sort 쿼리 추가 및 nested route 이용해 조건부 렌더링

--- section20/practice-project/src/pages/QuoteDetail.js
+++ section20/practice-project/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Route, useParams, Link } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -21,6 +21,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/`} exact>
+        <div className="centered">
+          <Link
+            className="btn--flat"
+            to={`/quotes/${params.quoteId}/comments`}
+          ></Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
