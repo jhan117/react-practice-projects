9c6ca2657f0a2258fd690715eed866049e1288fc
2001Kaye <jhan117@naver.com>
2023-01-04 17:38:24 +0900

    Feat: Head meta 추가

--- section23/bigger-project/pages/index.js
+++ section23/bigger-project/pages/index.js
@@ -1,9 +1,22 @@
 import { MongoClient } from "mongodb";
+import { Fragment } from "react";
+import Head from "next/head";
 
 import MeetupList from "../components/meetups/MeetupList";
 
 const HomePage = (props) => {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
 };
 
 // export const getServerSideProps = async (context) => {
