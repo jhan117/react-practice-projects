c23fdc8ac0fbf61291f8b2eca061112baeae206d
2001Kaye <jhan117@naver.com>
2023-01-04 15:52:09 +0900

    Feat: SSG에서 매개변수 사용하기 및 MongoDB 연결

--- section23/bigger-project/pages/[meetupId]/index.js
+++ section23/bigger-project/pages/[meetupId]/index.js
@@ -4,4 +4,42 @@ function MeetupDetails() {
   return <MeetupDetail image="" title="" address="" description="" />;
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image: "",
+        title: "",
+        address: "",
+        description: "",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
