b91742748aebdb5ee31fbe8d4ef7a1cffebed4d6
2001Kaye <jhan117@naver.com>
2023-01-05 10:29:41 +0900

    Feat: useStore에 새로운 상태 추가

--- section25/src/index.js
+++ section25/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import configureProductsStore from "./hooks-store/products-store";
+import configureCounterStore from "./hooks-store/counter-store";
 
 import "./index.css";
 import App from "./App";
 
 configureProductsStore();
+configureCounterStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
